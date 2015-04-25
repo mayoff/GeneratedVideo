5ca552538558beee515b45f221b17b7ecee5ac03
Rob Mayoff <git@rob.dqd.com>
2015-04-25 13:24:28 -0500

    Initial Commit

---
+++ makeMovie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  makeMovie
+//
+//  Created by Rob Mayoff on 4/25/15.
+//  Copyright (c) 2015 Rob Mayoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
